a7fb00a997fb08c9e3ff3f21cdc2c1d6f76a0579
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-05-05 17:38:07 -0300

    Loops

---
+++ javascript/aula1/loops.js
@@ -0,0 +1,19 @@
+// for
+
+for(let i = 0; i <= 5; i++){
+    console.log(i)
+}
+
+// while
+let i = 0
+while (i <= 5) {
+  console.log(i)
+  i++
+}
+
+// for of
+const numbers = [1, 2, 3, 4, 5]
+
+for (const num of numbers) {
+  console.log(num)
+}
