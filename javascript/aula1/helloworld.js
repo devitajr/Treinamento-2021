deff896657774314128cfb0c5fc01314951a0b58
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-05-05 16:41:31 -0300

    Condicionais

--- javascript/aula1/helloworld.js
+++ javascript/aula1/helloworld.js
@@ -7,3 +7,9 @@ console.log("hello world")
 Comentar 
 bloco
 */
+
+// bizu Comentar codigo VsCode: Ctrl + K + C
+// bizu Descomentar codigo VsCode: Ctrl + K + U
+
+// bizu Identar codigo: Ctrl + Shift + I
+          console.log('Codigo mal identado')
